0d171e62c6b5fd160feea8df4e7e40a44f758ab2
Billy Dorman <b.d.dorman12@gmail.com>
2021-07-16 12:18:27 -0500

    changed db to houston astros

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,21 @@
-DROP DATABASE IF EXISTS companyDB;
+DROP DATABASE IF EXISTS astrosDB;
 
-CREATE DATABASE companyDB;
+CREATE DATABASE astrosDB;
 
-USE companyDB;
+USE astrosDB;
 
 DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS job;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     deptName VARCHAR(30) NOT NULL
     PRIMARY KEY(id)
 );
 
 CREATE TABLE job (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     deptID INTEGER,
@@ -23,10 +23,55 @@ CREATE TABLE job (
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     firstName VARCHAR(30) NOT NULL,
     lastName VARCHAR(30) NOT NULL,
-    jobID INTEGER,
+    jobID VARCHAR(30),
     managerID INTEGER,
     PRIMARY KEY(id)
 );
+
+INSERT INTO department (deptName)
+VALUES 
+('Player'),
+('Coach'),
+('Executive Office'),
+('Baseball Operations');
+
+INSERT INTO job (title, salary, deptID)
+VALUES
+('First Base', 10000000, 1),
+('Second Base', 9000000, 1),
+('Third Base', 15000000, 1),
+('Short Stop', 30000000, 1),
+('Pitcher', 20000000, 1),
+('Catcher', 12500000, 1),
+('Right Field', 17500000, 1),
+('Left Field', 10000000, 1),
+('Center Field', 2500000, 1),
+('Head Coach', 3000000, 2),
+('Assistant Coach', 500000, 2),
+('Head GM', 10000000, 3),
+('Assistant GM', 2000000, 3),
+('Owner', 100000000, 4),
+('Vice President', 50000000, 4);
+
+
+INSERT INTO employee (firstName, lastName, jobID, managerID)
+VALUES
+('Yuli', 'Gurriel', '1B', null),
+('Jose', 'Altuve', '2B', null),
+('Alex', 'Bregman', '3B', null),
+('Carlos', 'Correa', 'SS', null),
+('Zack', 'Greinke', 'P', null),
+('Martin', 'Maldonado', 'C', null),
+('Kyle', 'Tucker', 'RF', null),
+('Michael', 'Brantley', 'LF', null),
+('Myles', 'Straw', 'CF', null),
+('Dusty', 'Baker', 'HC', 4),
+('Joe', 'Espada', 'AC', null),
+('Alex', 'Cintron', 'AC', null),
+('James', 'Click', 'GM', 3),
+('Brandon', 'Taubman', 'AGM', 3),
+('Jim', 'Crane', 'OW', 1),
+('Creighton', 'Kahoalii', 'VP', 2);
--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+// require dependencies
+
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 
+//create mysql connection
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: '12345678',
-    database: 'companyDB',
+    database: 'astrosDB',
   });
 
   connection.connect((err) => {
       if(err) throw err;
-      console.log(`connected as id ${connection.threadID}`);
+      console.log("Welcome to Houston Astros Employee-O-Matic!");
   });
+
+  //create inquirer prompts
